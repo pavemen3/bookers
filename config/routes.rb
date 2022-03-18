a54de3b43ab5d692ffc0ad8c7b53254b664f5fc8
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-18 00:13:21 +0000

    [finish]課題：アプリケーションを作成してみよう：基礎編

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   root "homes#top"
   resources :books
-  # get "books"  => "books#index"
-  # get "books/new"  => "books#new", as: "new_book"
-  # get "books/:id"  => "books#show", as: "show_book"
-
-  # post "books" => "books#create"
-  # get "books/:id/edit"  => "books#edit", as: "edit_book"
-  # patch "books/:id" => "books#update", as:"update_book"
-  # delete "books/:id" => "books#destroy"
 end
